fb0e175fd6d7b4c9eaed7de133dfd4232fa2674c
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2022-11-27 12:12:04 -0800

    header and footer styling and logo added. added background theme for the app. styled the homepage recipe cards and made them render with db query. still trying to figure out how to query categories.

--- client/src/App.js
+++ client/src/App.js
@@ -41,6 +41,15 @@ const client = new ApolloClient({
     cache: new InMemoryCache(),
 });
 
+const styles = {
+  headerFooter: {
+    backgroundColor: '#fffbf3'
+  },
+  background: {
+    backgroundColor: '#688774'
+  }
+}
+
 function App(){
     const [currentPage, setCurrentPage] = useState("Homepage");
 
@@ -49,17 +58,21 @@ function App(){
     <ApolloProvider client={client}>
        <Router>
       <div>
-        <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
-        <Routes>
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/" element={<Homepage />} />
-          <Route exact path="/userprofile" element={<UserProfile />} />
-          <Route exact path="/recipe:recipeId" element={<IndividualRecipe />} />
-          <Route exact path="/addrecipe" element={<AddRecipe />} />
-          <Route path="*" element={<Login />} />
-        </Routes>
-        <footer>
+        <div style={styles.headerFooter}>
+          <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
+        </div>
+        <div style={styles.background}>
+          <Routes>
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/homepage" element={<Homepage />} />
+            <Route exact path="/userprofile" element={<UserProfile />} />
+            <Route exact path="/recipe/:recipeId" element={<IndividualRecipe />} />
+            <Route exact path="/addrecipe" element={<AddRecipe />} />
+            <Route path="*" element={<Login />} />
+          </Routes>
+        </div>
+        <footer style={styles.headerFooter}>
           <Footer />
         </footer>
       </div>
