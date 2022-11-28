7a2f029571dce6adb5f28915321185f6aa74a410
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2022-11-28 09:53:20 -0800

    Added useMutation code figured out the 400 network request, fixed mutations.js to accept the primitives that I was providing in the addrecipe.js code. the mutation uses hard code, so now just need to figure out how to access values from the form and add them to the usemutation to accept user input

--- client/src/components/pages/addRecipe.js
+++ client/src/components/pages/addRecipe.js
@@ -4,7 +4,9 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import '../../styles/test.css'
-
+import { ADD_RECIPE } from '../../utils/mutations';
+import { useMutation } from '@apollo/client';
+import Auth from '../../utils/auth';
 
 
 
@@ -46,6 +48,8 @@ const styles = {
 }
 
 export default function AddRecipe() {
+  const [createRecipe, { error }] = useMutation(ADD_RECIPE);
+
   const inputArr = [
     {
       id: 1,
@@ -54,6 +58,39 @@ export default function AddRecipe() {
       className: "mb-3"
     }
   ];
+  const token = localStorage.getItem('id_token');
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    // try {
+    //   const { data } = await createRecipe({
+    //     variables: {
+    //       recipeName: "Fish",
+    //       recipeCategory: "Dinner",
+    //       recipeInstructions: "These are the steps",
+    //       recipeDescription: "This is a fish recipe",
+    //       recipeAuthor: Auth.getProfile().data.username,
+    //       recipeIngredients: "So many ingredients in this dish",
+    //       recipeImages: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/5/4/1/FNM_060112-Duff-Goldman-Spanish-Style-Grilled-Fish-recipe_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371606208411.jpeg",
+      
+    //     }
+    //   })
+    // } catch (e) {
+    //   console.log(e);
+    // }
+  } 
+  const [titleState, setTitleState] = useState('');
+  const handleTitleChange = function(e) {
+    setTitleState(e.target.value);
+  };
+  const [categoryState, setCategoryState] = useState('');
+  const handleCategoryChange = function(e) {
+    setCategoryState(e.target.value);
+  };
+  const [ingredientState, setingredientState] = useState('');
+  const handleIngredientChange = function (e) {
+    setingredientState(e.target.value);
+  };
+
 
   const [arr, setArr] = useState(inputArr);
 
@@ -70,33 +107,67 @@ export default function AddRecipe() {
     });
   };
 
-  const handleChange = e => {
-    e.preventDefault();
+  // const handleChange = e => {
+  //   e.preventDefault();
 
-    const index = e.target.id;
-    setArr(s => {
-      const newArr = s.slice();
-      newArr[index].value = e.target.value;
+  //   const index = e.target.id;
+  //   setArr(s => {
+  //     const newArr = s.slice();
+  //     newArr[index].value = e.target.value;
 
-      return newArr;
-    });
-  };
+  //     return newArr;
+  //   });
+  // };
   return (
     <div style={styles.AddRecipeContainer}>
+
+
+      <h1 style={styles.addRecipeTitle}>Add A New Recipe</h1>
+      <div style={styles.titleContainer}>
+        <h3>Recipe Title:</h3>
+        <InputGroup size="sm" className="mb-3" style={styles.titleInput}>
+          <Form.Control
+            aria-label="Small"
+            aria-describedby="inputGroup-sizing-sm"
+            placeholder="Type recipe title here..."
+            value={titleState}
+            onChange={handleTitleChange}
+          />
+        </InputGroup>
+      </div>
       <div>
-        <button onClick={addInput}>+</button>
+        <h3>Recipe Description:</h3>
+        <InputGroup>
+          <Form.Control
+            as="textarea"
+            aria-label="With textarea"
+            placeholder="Type recipe description here..."
+          />
+        </InputGroup>
+      </div><br></br>
+      {/* <InputGroup className="mb-3">
+      <InputGroup.Text>First and last name</InputGroup.Text>
+      <Form.Control aria-label="First name" />
+      <Form.Control aria-label="Last name" />
+    </InputGroup> */}
+
+      <div>
+      <div>
+        <Button variant="primary" onClick={addInput}>+ Add Another Ingredient</Button>
         {arr.map((item, i) => {
           return (
             <div>
-              <InputGroup onChange={handleChange} size={item.size} className={item.className} value={item.value} id={i} style={styles.ingredientInput}>
+              <InputGroup size={item.size} className={item.className} id={i} style={styles.ingredientInput}>
                 <Form.Control
                   aria-label="Small"
                   aria-describedby="inputGroup-sizing-sm"
                   placeholder="Type ingredient name here..."
+                  value={ingredientState}
+                  onChange={handleIngredientChange}
                 />
               </InputGroup>
 
-              <InputGroup onChange={handleChange} size={item.size} className={item.className} value={item.value} id={i} style={styles.quantInput}>
+              <InputGroup size={item.size} className={item.className} id={i} style={styles.quantInput}>
                 <Form.Control
                   aria-label="Small"
                   aria-describedby="inputGroup-sizing-sm"
@@ -108,43 +179,7 @@ export default function AddRecipe() {
         })}
       </div>
 
-      <h1 style={styles.addRecipeTitle}>Add A New Recipe</h1>
-      <div style={styles.titleContainer}>
-        <h3>Recipe Title:</h3>
-        <InputGroup size="sm" className="mb-3" style={styles.titleInput}>
-          <Form.Control
-            aria-label="Small"
-            aria-describedby="inputGroup-sizing-sm"
-            placeholder="Type recipe title here..."
-          />
-        </InputGroup>
-      </div>
-
-      <div>
-        <div style={styles.recipeIngredients}>
-          <div style={styles.recipeItemInput}>
-            <h5>Ingredients:</h5>
-            <InputGroup size="sm" className="mb-3" style={styles.ingredientInput}>
-              <Form.Control
-                aria-label="Small"
-                aria-describedby="inputGroup-sizing-sm"
-                placeholder="Type ingredient name here..."
-              />
-            </InputGroup>
-          </div>
-          <div style={styles.recipeItemInput}>
-            <h5>Quantity:</h5>
-            <InputGroup size="sm" className="mb-3" style={styles.quantInput}>
-              <Form.Control
-                aria-label="Small"
-                aria-describedby="inputGroup-sizing-sm"
-                placeholder="Type ingredient quantity here..."
-              />
-            </InputGroup>
-          </div>
-        </div>
-
-        <Button variant="primary">+ Add Another Ingredient</Button>{' '}
+        
 
         <div style={styles.recipeItemInput}>
           <h5>Recipe Image:</h5>
@@ -157,11 +192,11 @@ export default function AddRecipe() {
           </InputGroup>
 
         </div>
-        <Form.Select aria-label="Default select example">
+        <Form.Select aria-label="Default select example" onChange={handleCategoryChange}>
           <option>Select Recipe Category</option>
-          <option value="1">Breakfast</option>
-          <option value="2">Lunch</option>
-          <option value="3">Dinner</option>
+          <option value="Breakfast">Breakfast</option>
+          <option value="Lunch">Lunch</option>
+          <option value="Dinner">Dinner</option>
         </Form.Select>
       </div>
 
@@ -177,7 +212,7 @@ export default function AddRecipe() {
       </div>
 
       <div>
-        <Button variant="primary">Submit Recipe</Button>{' '}
+        <Button variant="primary" onClick={onSubmit}>Submit Recipe</Button>
       </div>
 
     </div>
