4cbcbae09b71e23d0b59822affde9dd14c851088
asmitt8373 <austinsmittle8373@gmail.com>
2022-11-28 21:27:29 -0800

    finishing touches with individual recipe

--- client/src/components/pages/individualRecipe.js
+++ client/src/components/pages/individualRecipe.js
@@ -5,17 +5,21 @@ import { useParams } from "react-router-dom";
 
 const styles = {
   title: {
-    margin: "2vh 8vw 0vh 5vw",
+    margin: "2vh 8vw 4vh 5vw",
     textAlign: "center",
   },
   image: {
-    textAlign: "center",
-    height: "20vh",
-    border: "2px solid",
-    borderRadius: "5px",
     padding: "5px",
     margin: "3vh 3vw 3vh 3vw",
+  
+    
   },
+  imageStyle: {
+    objectFit: "cover",
+    maxWidth: "40vw",
+    maxHeight: "40vh",
+  },
+
   time: {
     textAlign: "center",
     height: "20vh",
@@ -23,6 +27,8 @@ const styles = {
     borderRadius: "5px",
     padding: "5px",
     margin: "3vh 3vw 3vh 3vw",
+    width: "32vw",
+    
   },
   instructions: {
     margin: "3vh 3vw 3vh 3vw",
@@ -36,11 +42,13 @@ const styles = {
   },
   ingredients: {
     textAlign: "center",
-    height: "20vh",
-    width: "60vw",
+    height: "40vh",
+    width: "50vw",
     border: "2px solid",
     borderRadius: "5px",
     padding: "5px",
+    margin: "3vh 3vw"
+    
   },
   comment: {},
 };
@@ -48,19 +56,21 @@ function IndividualRecipe() {
   const { recipeId } = useParams();
   const { loading, data } = useQuery(individualRecipe, {
     variables: { id: recipeId },
-  
+    
   });
-  // console.log(data)
-  // console.log(recipeId)
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  console.log(data)
   return (
     <div className="wrapper">
       <h1 className="title" style={styles.title}>
         {data.recipe.recipeName}
       </h1>
 
-      <div className="row">
+      <div className="row" id="individual">
         <div className="col-6 col-sm-4 image" style={styles.image}>
-         <img src={data.recipe.recipeImages}></img>
+         <img style={styles.imageStyle} src={data.recipe.recipeImages}></img>
         </div>
         <div className="col-6 col-sm-4 ingredients" style={styles.ingredients}>
           <p>{data.recipe.recipeIngredients}</p>
@@ -68,15 +78,15 @@ function IndividualRecipe() {
 
         <div className="w-100"></div>
         <div className="col-6 col-sm-4 time" style={styles.time}>
-          <h1>{data.recipe.recipeAuthor}</h1>
-          <h1>{data.recipe.recipeCategory}</h1>
+          <h1>Created By: {data.recipe.recipeAuthor}</h1>
+          <h1>Category: {data.recipe.recipeCategory}</h1>
         </div>
       </div>
       <div className="row instructions" style={styles.instructions}>
         <p>{data.recipe.recipeInstructions}</p>
       </div>
 
-      <form role="form" className="comment" style={styles.comment}>
+      <form role="form">
         <div class="form-group">
           <textarea class="form-control" rows="3" required></textarea>
         </div>
@@ -84,6 +94,7 @@ function IndividualRecipe() {
           Submit
         </button>
       </form>
+      <div className="comment" style={styles.comment}><p>comments: {data.recipe.comments[0].commentText}</p></div>
     </div>
   );
 }
